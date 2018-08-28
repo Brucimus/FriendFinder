08be003e761720849d04e52ae15edfc5659752c1
Brucimus <Brucimus@gmail.com>
2018-08-27 22:26:54 -0700

    added modal and finished friend calc

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,15 +8,26 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
         var match;
+        var initFriendVal = 100;
         var temp = 0;
         console.log(req.body);
+
+        //iterate through available friends
         for (var i = 0; i < peopleDataArray.length; i++) {
+
+            //iterate through scores
             for (var j = 0; j < peopleDataArray[i].scores.length; j++) {
-                temp += peopleDataArray[i].scores[j] - req.body.scores[j];
+                temp += Math.abs(peopleDataArray[i].scores[j] - req.body.scores[j]);
                 console.log(temp);
             };
+            if (temp < initFriendVal) {
+                initFriendVal = temp;
+                match = peopleDataArray[i];
+                temp = 0;
+            }
         }
+        console.log(match.name);
         peopleDataArray.push(req.body);
-        res.json(true);
+        res.json(match);
     });
 }
